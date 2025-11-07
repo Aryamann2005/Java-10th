b938a0895bfae8738075d3ba7ad6f70796751dc2
Aryamann Thakur <141225575+Aryamann2005@users.noreply.github.com>
2025-11-07 23:13:02 +0530

    Create Course.java

---
+++ src/main/java/com/example/osms/model/Course.java
@@ -0,0 +1,57 @@
+package com.example.osms.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
+    private Integer courseId;
+
+    @Column(name = "course_name", nullable = false)
+    private String courseName;
+
+    private String duration;
+
+    public Course() {}
+
+    public Course(String courseName, String duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", duration='" + duration + '\'' +
+                '}';
+    }
+}
